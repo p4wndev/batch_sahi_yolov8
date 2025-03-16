5ca56ee280274a14dacd3f5a0796a7e61d65f203
Thai Phu An <141134456+p4wndev@users.noreply.github.com>
2025-03-16 18:58:49 +0700

    batch sahi

---
+++ README.md
@@ -0,0 +1,18 @@
+## Usage
+
+```
+from sahi_batched.models import Yolov8DetectionModelCustom
+from sahi_batched import get_sliced_prediction_batch
+```
+
+```
+Yolov8DetectionModel(
+    model_path="./yolov8n.pt",
+    confidence_threshold=0.3,
+    device="cuda:0",
+    ...
+)
+
+
+get_sliced_prediction_batch(...)
+```
---
+++ batch_sahi/models/yolov8.py
@@ -0,0 +1,24 @@
+from sahi.models.base import DetectionModel
+from typing import List
+import numpy as np
+
+class Yolov8DetectionModelCustom(DetectionModel):
+    def perform_inference(self, images: List[np.ndarray]):
+        """
+        Prediction is performed using self.model and the prediction result is set to self._original_predictions.
+        Args:
+            image: np.ndarray
+                A numpy array that contains the image to be predicted. 3 channel image should be in RGB order.
+        """
+
+        # Confirm model is loaded
+        if self.model is None:
+            raise ValueError("Model is not loaded, load it by calling .load_model()")
+
+        prediction_result = self.model.predict(source=images, verbose=False, device=self.device)
+
+        prediction_result = [
+            result.boxes.data[result.boxes.data[:, 4] >= self.confidence_threshold] for result in prediction_result
+        ]
+
+        self._original_predictions = prediction_result
